fe8b1873c7359f81c304e242f6baf1be3dbdff3e
Tim Firman <tkfirman@gmail.com>
2018-01-25 09:23:35 -0800

    adds check for draw, stops clicking after a win

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,5 @@
 var cells = [0,0,0,0,0,0,0,0,0];
+var gameOver = false;
 
 function turn(t) {
   var t = 0;
@@ -7,9 +8,12 @@ function turn(t) {
 }
 
 function checkThreeForWin(i,j,k){
-  if (cells[i]!==0 && cells[i]===cells[j] && cells[j]===cells[k]) {
-    if (cells[i]===1) {alert ('X wins!');}
-    if (cells[i]===2) {alert ('O wins!');}
+  if (cells[i]===1 && cells[j]===1 && cells[k]===1) {
+    alert ('X wins!');
+    gameOver = true;
+  } else if (cells[i]===2 && cells[j]===2 && cells[k]===2) {
+    alert ('O wins!');
+    gameOver = true;
   };
 }
 
@@ -25,7 +29,7 @@ function checkForWin(){
 }
 
 function clickedCell(i) {
-  if (cells[i] === 0) {
+  if (cells[i] === 0 && !gameOver) {
     if (turn() % 2 === 0) {
         $("#cell"+i).text("X");
         cells[i] = 1;
@@ -34,6 +38,10 @@ function clickedCell(i) {
         cells[i] = 2;
     };
     checkForWin();
+    if(turn()===9 && !gameOver) {
+      alert ('Game was a draw!')
+      gameOver = true;
+    };
   };
 }
 
@@ -46,64 +54,3 @@ $(document).ready(function(){
     });
   };
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $("#cell0").click(function(event){
-//   event.preventDefault();
-//   if (cellArray[0] === 0) turn = clickedCell(cellArray,turn,0);
-// });
-// $("#cell1").click(function(event){
-//   event.preventDefault();
-//   if (cellArray[1] === 0) turn = clickedCell(cellArray,turn,1);
-// });
-// $("#cell2").click(function(event){
-//   event.preventDefault();
-//   if (cellArray[2] === 0) turn = clickedCell(cellArray,turn,2);
-// });
-// $("#cell3").click(function(event){
-//   event.preventDefault();
-//   if (cellArray[3] === 0) turn = clickedCell(cellArray,turn,3);
-// });
-// $("#cell4").click(function(event){
-//   event.preventDefault();
-//   if (cellArray[4] === 0) turn = clickedCell(cellArray,turn,4);
-// });
-// $("#cell5").click(function(event){
-//   event.preventDefault();
-//   if (cellArray[5] === 0) turn = clickedCell(cellArray,turn,5);
-// });
-// $("#cell6").click(function(event){
-//   event.preventDefault();
-//   if (cellArray[6] === 0) turn = clickedCell(cellArray,turn,6);
-// });
-// $("#cell7").click(function(event){
-//   event.preventDefault();
-//   if (cellArray[7] === 0) turn = clickedCell(cellArray,turn,7);
-// });
-// $("#cell8").click(function(event){
-//   event.preventDefault();
-//   if (cellArray[8] === 0) turn = clickedCell(cellArray,turn,8);
-// });
